d1e0c289cd2ea69124ed0fcdfff1ef5f55d3336c
duudafs <dudascomparin@gmail.com>
2026-03-22 19:27:36 -0300

    service finalizado

---
+++ src/main/java/com/mh/GerenciamentoDeTarefas/services/TarefaService.java
@@ -0,0 +1,31 @@
+package com.mh.GerenciamentoDeTarefas.services;
+
+import com.mh.GerenciamentoDeTarefas.models.TarefaModel;
+import com.mh.GerenciamentoDeTarefas.repositories.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TarefaService {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    public List<TarefaModel> buscarTarefas(){
+        return tarefaRepository.findAll();
+    }
+
+    public TarefaModel criarTarefa(TarefaModel tarefaModel) {
+        return tarefaRepository.save(tarefaModel);
+    }
+
+    public TarefaModel buscarId(Long id) {
+        return tarefaRepository.findById(id).get();
+    }
+
+    public void deletarTarefa(Long id) {
+        tarefaRepository.deleteById(id);
+    }
+}
